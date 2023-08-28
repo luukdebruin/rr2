66e7bf843d71e83642ff8eef580cf3674ee7d9b7
Luuk de Bruin <luuk.debruin15@gmail.com>
2023-08-28 17:34:29 +0200

    coming soon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function Index() {
-	return <div></div>
+	return (
+		<div className="w-screen h-screen flex justify-center items-center">
+			<h1 className="font-calendas text-2xl">Coming soon</h1>
+		</div>
+	)
 }
